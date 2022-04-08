6216ab0432735c9b10a2d825935f6e24b4152d2c
Ken Payne <ken@meltano.com>
2022-04-08 17:00:11 +0100

    update metadata and readme

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="files-dbt",
-    version="0.3",
-    description="Meltano project files for dbt",
+    name="files-dbt-snowflake",
+    version="0.1",
+    description="Meltano project files for dbt-snowflake",
     packages=find_packages(),
     package_data={
         "bundle": [
